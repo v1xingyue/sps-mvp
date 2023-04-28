d0a4309c8092748b64ae4ec9dbc0ffd2fb204e5a
v1xingyue <qixingyue@126.com>
2023-04-28 16:15:36 +0800

    basic profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
+import { useWallet } from "@suiet/wallet-kit";
+import { getProfile } from "../firebase/index";
+
 const Profile = () => {
+    const { connected, account } = useWallet();
+    const [profile, updateProfile] = useState<any>(null);
+    useEffect(() => {
+        if (connected && account) {
+            const address = account.address;
+            console.log(address);
+            (async () => {
+                const profile = await getProfile(address);
+                console.log(address, profile);
+                updateProfile(profile);
+            })();
+        }
+    }, [account, connected])
     return (
-        <p>Profile page </p>
+        <>
+            {profile == null ? null : (
+                <>
+                    <h2>Profile page </h2>
+                    <ul>
+                        <li>Address : {profile.address}</li>
+                        <li>Github : {profile.github}</li>
+                        <li>Register time : {new Date(profile.register_time.seconds * 1000).toLocaleString()}</li>
+                    </ul>
+                </>
+            )}
+
+        </>
+
     )
 }
 
